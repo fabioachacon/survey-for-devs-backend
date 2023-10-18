1dcdee4ec3df104185c0784e2a7cc513aba81810
Fábio de Andrade <fabioac71@gmail.com>
2023-10-18 19:52:26 -0300

    feat:  ensure SignUPController returns 400 if an Invalid Email is provided

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -2,8 +2,16 @@ import { MissingParamError } from "../errors/MissingParamError";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { getBadRequest } from "../helpers/mock-bad-request";
 import { Controller } from "../protocols/controller";
+import { EmailValidor } from "../protocols/email-validator";
+import { InvalidParamError } from "../errors/InvalidParamError";
 
 export class SignUpController implements Controller {
+  private readonly emailValidor: EmailValidor;
+
+  constructor(emailValidor: EmailValidor) {
+    this.emailValidor = emailValidor;
+  }
+
   public async handle(request: HttpRequest): Promise<HttpResponse | undefined> {
     const requiredFields = [
       "name",
@@ -16,6 +24,11 @@ export class SignUpController implements Controller {
       if (!request.body[field]) {
         return getBadRequest(new MissingParamError(field));
       }
+    }
+
+    const isValidEmail = this.emailValidor.isValid(request.body.email);
+    if (!isValidEmail) {
+      return getBadRequest(new InvalidParamError("email"));
     }
   }
 }
---
+++ src/presentation/errors/InvalidParamError.ts
@@ -0,0 +1,7 @@
+export class InvalidParamError extends Error {
+  constructor(name: string) {
+    super(`Invalid param: ${name}`);
+
+    this.name = "InvalidParamError";
+  }
+}

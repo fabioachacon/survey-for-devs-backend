de1101872546524aa9254bd9978e843422b2d757
Fábio de Andrade <fabioac71@gmail.com>
2023-10-18 17:18:59 -0300

    refactor: moved duplicated code do a helper method

--- src/presentation/controllers/test/signup-controller-spy.ts
+++ src/presentation/controllers/test/signup-controller-spy.ts
@@ -1,8 +1,8 @@
-import { MissingParamError } from "../errors/MissingParamErro";
-import { HttpRequest, HttpResponse } from "../protocols/http";
-import { getMockedBadRequest } from "../test/mock-bad-request";
+import { MissingParamError } from "../../errors/MissingParamErro";
+import { HttpRequest, HttpResponse } from "../../protocols/http";
+import { getMockedBadRequest } from "../../test/mock-bad-request";
 
-export class SignUpController {
+export class SignUpControllerSpy {
   public async handle(request: HttpRequest): Promise<HttpResponse | undefined> {
     if (!request?.body.name) {
       return getMockedBadRequest(new MissingParamError("name"));

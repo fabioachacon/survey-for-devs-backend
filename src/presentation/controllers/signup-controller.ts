d464ff855b03ca7c00a2126d14e60e805c133fad
Fábio de Andrade <fabioac71@gmail.com>
2023-10-18 18:43:42 -0300

    feat: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -4,7 +4,7 @@ import { getBadRequest } from "../helpers/mock-bad-request";
 
 export class SignUpController {
   public async handle(request: HttpRequest): Promise<HttpResponse | undefined> {
-    const requiredFields = ["name", "email"];
+    const requiredFields = ["name", "email", "password"];
 
     for (const field of requiredFields) {
       if (!request.body[field]) {

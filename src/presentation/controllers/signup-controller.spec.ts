f2ada4b4d3b41faf2005a6b557859201ee18a017
Fábio de Andrade <fabioac71@gmail.com>
2023-10-18 16:35:43 -0300

    test: ensure SignUpController returns an Error if no email is provided

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -16,4 +16,21 @@ describe("SignUp Controller", () => {
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual(new Error("Missing param: name"));
   });
+
+  test("Should retorn 400 if no email is provided", async () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        passord: "any",
+        passwordConfirmation: "any",
+      },
+    };
+
+    const response = await sut.handle(httpRequest);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new Error("Missing param: email"));
+  });
 });

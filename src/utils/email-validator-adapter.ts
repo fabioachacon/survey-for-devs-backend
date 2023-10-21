5d9fda41a82d99303d0d6127b2f8843dbca91a45
Fábio de Andrade <fabioac71@gmail.com>
2023-10-21 14:52:04 -0300

    style: remove boolean type

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -2,7 +2,7 @@ import validator from "validator";
 import { EmailValidator } from "../presentation/protocols/email-validator";
 
 export class EmailValidatorAdapter implements EmailValidator {
-  isValid(email: string): boolean {
+  isValid(email: string) {
     return validator.isEmail(email);
   }
 }

1d41708fd8c93d291a4472137e5c70c0bb6c37b5
Fábio de Andrade <fabioac71@gmail.com>
2023-10-25 00:45:48 -0300

    test: ensure DbAddAccount calls AddAccountRepository.add with correct values

---
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,61 @@
+import { DbAddAccount } from "./db-add-account";
+import { makeAddAccountRepositoryStub, makeEncrypterStub } from "./test/stubs";
+
+const getSut = () => {
+  const encryperStub = makeEncrypterStub();
+  const repositoryStub = makeAddAccountRepositoryStub();
+
+  return {
+    sut: new DbAddAccount(encryperStub, repositoryStub),
+    encryperStub,
+    repositoryStub,
+  };
+};
+
+const getMockedAccountData = () => {
+  return {
+    name: "valid_name",
+    email: "valid_email",
+    password: "any_password",
+  };
+};
+
+describe("DbAddAccount", () => {
+  test("Should call Encrypter.encrypt with password", async () => {
+    const { sut, encryperStub } = getSut();
+
+    const encryptSpy = jest.spyOn(encryperStub, "encrypt");
+
+    const accoutData = getMockedAccountData();
+    await sut.add(accoutData);
+
+    expect(encryptSpy).toHaveBeenCalledWith("any_password");
+  });
+
+  test("Should throw Encrypter.encrypt throws", async () => {
+    const { sut, encryperStub } = getSut();
+
+    jest.spyOn(encryperStub, "encrypt").mockImplementationOnce(async () => {
+      throw new Error();
+    });
+
+    const accountData = getMockedAccountData();
+    const promise = sut.add(accountData);
+
+    expect(promise).rejects.toThrow(new Error());
+  });
+
+  test("Should call AddAccountRepository with correct values", async () => {
+    const { sut, repositoryStub } = getSut();
+
+    const addAccountRepoSpy = jest.spyOn(repositoryStub, "add");
+    const accountData = getMockedAccountData();
+
+    await sut.add(accountData);
+    expect(addAccountRepoSpy).toHaveBeenCalledWith({
+      name: "valid_name",
+      email: "valid_email",
+      password: "hashed_password",
+    });
+  });
+});

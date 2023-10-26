a25ffd0c75decb8846e1903a6a5a0699078718d9
Fábio de Andrade <fabioac71@gmail.com>
2023-10-25 21:54:23 -0300

    feat: add cors middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { bodyParser } from "../middlewares/parser/body-parser";
+import { cors } from "../middlewares/cors/cors";
 
 const setServerMiddlewares = (server: Express) => {
   server.use(bodyParser);
+  server.use(cors);
 };
 
 export default setServerMiddlewares;

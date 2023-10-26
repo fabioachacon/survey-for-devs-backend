11d03decb472f57e09cd52cf9f5977c998a70031
Fábio de Andrade <fabioac71@gmail.com>
2023-10-25 21:54:02 -0300

    test: ensure request body is parsed as json

---
+++ src/main/middlewares/parser/body-parser.test.ts
@@ -0,0 +1,15 @@
+import request from "supertest";
+import server from "../../config/server-config";
+
+describe("BodyParserMiddleware", () => {
+  test("Should parse request body as json", async () => {
+    server.post("/test_body_parser", (req, res) => {
+      res.send(req.body);
+    });
+
+    await request(server)
+      .post("/test_body_parser")
+      .send({ name: "any" })
+      .expect({ name: "any" });
+  });
+});

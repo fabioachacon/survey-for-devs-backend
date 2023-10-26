3cd529e03a089e1e641f60d2ca018d04f21811f0
Fábio de Andrade <fabioac71@gmail.com>
2023-10-25 22:41:01 -0300

    feat: ensure AccountMongoRepository.add returns correct value

---
+++ src/infra/database/mongodb/repository/account-repository-adapter.ts
@@ -0,0 +1,16 @@
+import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
+import { AccountModel } from "../../../../domain/models/account";
+import { AddAccountModel } from "../../../../domain/usecases/add-account";
+import { MongoHelper } from "../utils/mongo-helper";
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    const result = await accountCollection.insertOne(account);
+
+    return {
+      id: result.insertedId.toString(),
+      ...account,
+    };
+  }
+}

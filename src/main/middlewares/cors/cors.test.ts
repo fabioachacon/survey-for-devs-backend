2ac73fd5e9ab1a19251bada1d047983b05c4d111
Fábio de Andrade <fabioac71@gmail.com>
2023-10-25 21:53:50 -0300

    test: ensure cors is enabled

---
+++ src/main/middlewares/cors/cors.test.ts
@@ -0,0 +1,16 @@
+import server from "../../config/server-config";
+import request from "supertest";
+
+describe("CORS middleware", () => {
+  test("Should enable cors", async () => {
+    server.get("/test_cors", (req, res) => {
+      res.send();
+    });
+
+    await request(server)
+      .get("/test_cors")
+      .expect("access-control-allow-origin", "*")
+      .expect("access-control-allow-methods", "*")
+      .expect("access-control-allow-headers", "*");
+  });
+});

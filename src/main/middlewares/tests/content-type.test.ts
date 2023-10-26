0897ce7ccab6eeb0ca6c4c2bf2925df4f09f7fe9
Fábio de Andrade <fabioac71@gmail.com>
2023-10-25 22:34:52 -0300

    style: change file structure

---
+++ src/main/middlewares/tests/content-type.test.ts
@@ -0,0 +1,25 @@
+import server from "../../config/server-config";
+import request from "supertest";
+
+describe("Content Type Middleware", () => {
+  test("Should return json as default content type", async () => {
+    server.get("/test_content_type", (req, res) => {
+      res.send();
+    });
+
+    await request(server)
+      .get("/test_content_type")
+      .expect("content-type", /json/);
+  });
+
+  test("Should return xml content type when enforced", async () => {
+    server.get("/test_xml_content_type", (req, res) => {
+      res.type("xml");
+      res.send("");
+    });
+
+    await request(server)
+      .get("/test_xml_content_type")
+      .expect("content-type", /xml/);
+  });
+});

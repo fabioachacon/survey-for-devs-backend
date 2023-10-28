4ecaf7a853ba3bdb56d5013d72e1716da555b117
Fábio de Andrade <fabioac71@gmail.com>
2023-10-28 14:45:46 -0300

    feat: add SignUp factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,15 @@
+import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
+import { AccountMongoRepository } from "../../infra/database/mongodb/repository/account-repository-adapter";
+import { BcryptAdapter } from "../../infra/encryption/bcrypt-adapter";
+import { SignUpController } from "../../presentation/controllers/signup/signup-controller";
+import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+
+export const makeSignUpController = () => {
+  const AddAccount = new DbAddAccount(
+    new BcryptAdapter(12),
+    new AccountMongoRepository()
+  );
+  const emailValidator = new EmailValidatorAdapter();
+
+  return new SignUpController(emailValidator, AddAccount);
+};
